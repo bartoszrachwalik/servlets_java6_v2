2ba857f769bccc5b57a1d7cd9ac0088f342c8431
Krzysztof Gonia <krzysztof.gonia@globallogic.com>
2018-03-08 18:58:50 +0100

    fix

--- src/main/java/org/sda/repository/UserRepository.java
+++ src/main/java/org/sda/repository/UserRepository.java
@@ -26,6 +26,7 @@ public class UserRepository {
     @PersistenceContext
     EntityManager em;
 
+    @Transactional
     public User findById(long id) {
         return em.find(User.class, id);
     }
